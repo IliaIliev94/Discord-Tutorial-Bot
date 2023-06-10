b50960bdf748c0ead982c89ca4ff8fec45c3af5d
Ilia Iliev <ilia_iliev94@abv.bg>
2023-06-10 18:13:47 +0300

    Created initial commands

---
+++ commands/user.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Provides information about the user.'),
+	async execute(interaction) {
+		return interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+	},
+};
